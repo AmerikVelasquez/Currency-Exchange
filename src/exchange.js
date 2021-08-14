1fab60a8682cb629ecdabe63ad6f260e4b663f5e
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-13 20:16:06 -0700

    add parameters for the api

--- src/exchange.js
+++ src/exchange.js
@@ -1,13 +1,13 @@
 export default class Exchange {
-  static async getExchange(){
+  static async getExchange(type,amount){
     try{
-      const response= await fetch(`https://v6.exchangerate-api.com/v6/key=${API_KEY.env}`);
+      const response= await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY.env}/pair/USD/${type}/${amount}`);
     if (!response.ok){
       throw Error(response.statusText);
     }
     return response.json();
     } catch (error) {
-      return error.message;
+      $('div#results').append(`<br><h1>${response.error}<h1><p>I don't know how you broke it but you managed to. Congragradulations???</p>`);
     }
   }
 }
