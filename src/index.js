e3b820a2cda58e608c4a910ff64466cbf5f77b13
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-13 20:19:01 -0700

    add functionality for specific api info to be displayed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Exchange from './exchange.js';
+
+function displayResults(response) {
+      $('div#results').prepend(`<br><h3>${response.conversion_result}</h3>`);
+}
+async function linkExchange(type, amount) {
+  let response = null;
+  let amount = $("amount");
+  switch(type) {
+    case "PHP":
+    case "JPY":
+    case "GBP":
+    case "EGP":
+    case "PLN":
+      response = await Exchange.getExchange(type, amount);
+    displayResults(response);
+  }
+}
+
+$(document).ready(function(){
+  $('button').click(function(){
+    let type = $('select#option').val();
+    let amount =$().val();
+    linkExchange(type, amount);
+  })
+})
