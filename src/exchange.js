bdef5acfe9717d11023ac9bc23d82e5667fbf2a6
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-08-22 00:48:34 -0700

    fix spacing issues

--- src/exchange.js
+++ src/exchange.js
@@ -2,12 +2,12 @@ export default class Exchange {
   static async getExchange(type,amount){
     try{
       const response= await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${type}/${amount}`);
-    if (!response.ok){
-      throw Error(response.statusText);
-    }
-    return response.json();
+      if (!response.ok){
+        throw Error(response.statusText);
+      }
+      return response.json();
     } catch (error) {
-     return error.message;
+      return error.message;
     }
   }
 }
